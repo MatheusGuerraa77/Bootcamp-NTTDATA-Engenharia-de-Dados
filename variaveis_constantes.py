9bd589f31ede6e2faf6d5adb650172867722fa7c
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2024-08-29 12:09:46 -0300

    Create variaveis_constantes.py

---
+++ variaveis_constantes.py
@@ -0,0 +1,7 @@
+#Variáveis e Constantes
+
+age, name = (19, 'Matheus')
+print(f'Meu nome é {name} e tenho {age} anos de idade!')
+
+BRAZILIAN_STATES =  ['SP', 'RJ', 'SC', 'RS']
+print(BRAZILIAN_STATES)
