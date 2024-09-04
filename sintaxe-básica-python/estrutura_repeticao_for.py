1c2b6903a03df1d40c7f41f684321b523dc6e412
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2024-09-04 01:16:32 -0300

    commit estrutura de repetição

---
+++ sintaxe-básica-python/estrutura_repeticao_for.py
@@ -0,0 +1,15 @@
+texto = ''
+VOGAIS = 'AEIOU'
+
+
+# exemplo utilizando um interável
+for letra in texto:
+    if letra.upper() in VOGAIS:
+        print(letra, end='')
+else:
+    print()# adiciona uma quebra de linha
+
+# exemplo utilizando a função built-in range
+for numero in range(0, 51, 5):
+    print(numero, end=' ')
+    
