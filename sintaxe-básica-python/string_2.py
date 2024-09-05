1d37b30a11c741fdb942790e93d6180cdd6456dc
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2024-09-05 19:08:30 -0300

    Create string_2.py

---
+++ sintaxe-básica-python/string_2.py
@@ -0,0 +1,8 @@
+nome = 'Matheus'
+idade = 28
+profissao = 'Programador'
+linguagem = 'Python'
+
+print('Nome: {1} Idade: {0}'.format(idade, nome))
+print('Nome: {nome} Idade: {idade}'.format(nome = nome, idade = idade))
+print('Nome: {name} Idade: {age}'.format(age = 28, name=nome))
