1fb93bf36d74faf3dead41376ed0d98910b958b9
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2024-09-04 23:46:27 -0300

    Create string_1.py

---
+++ sintaxe-básica-python/string_1.py
@@ -0,0 +1,18 @@
+nome = 'MaTHeus'
+
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+
+texto = '  Olá mundo!    '
+
+print(texto + '.')
+print(texto.strip() + '.')
+print(texto.rstrip() + '.')
+print(texto.lstrip() + '.')
+
+menu = 'Python'
+
+print('####' + menu + '####')
+print(menu.center(14, '#'))    
+print('-'.join(menu))
