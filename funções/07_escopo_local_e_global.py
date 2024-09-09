3d77a7e9274e4ca36330ddc6cfe89d58df6eab36
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2024-09-09 12:22:19 -0300

    commit funcoes

---
+++ funções/07_escopo_local_e_global.py
@@ -0,0 +1,18 @@
+salario = 2000
+
+def salario_bonus(bonus):
+    global salario
+    
+    lista_aux = lista.copy()
+    lista_aux.append(2)
+    print(f'lista aux={lista_aux}')
+    
+    salario += bonus
+    return salario
+
+
+lista = [1]
+
+salario_com_bonus = salario_bonus(500) # 2500
+print(salario_com_bonus)
+print(lista)
