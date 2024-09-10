8f7030e1b9f9cfa87056c6f1a121cd95ef116d84
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2024-09-10 16:17:49 -0300

    update listas/commit tuplas

---
+++ 02-tuplas/tuplas.py
@@ -0,0 +1,14 @@
+valores = (90, 79, 54, 32, 21)
+pontos = (100, 94.05, 86.8, 62)
+
+print(type(valores))
+print(type(pontos))
+
+tupla = (10, 20, 30, 40, 50)
+
+# Acessando elementos por índice
+primeiro_elemento = tupla[0]  # 10
+ultimo_elemento = tupla[-1]   # 50
+
+print(primeiro_elemento)
+print(ultimo_elemento)
