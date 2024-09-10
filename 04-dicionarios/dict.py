8cab353b970bf40e444b10596d6c137be79d17b4
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2024-09-10 18:17:28 -0300

    commit estrutura de dados

---
+++ 04-dicionarios/dict.py
@@ -0,0 +1,16 @@
+pessoa = {'nome': 'Guilherme', 'idade': 28}
+
+pessoa = dict(nome='Guilherme', idade=28)
+
+pessoa['telefone'] = '3333-1234' # {'nome': 'Guilherme', 'idade': 28, 'telefone': '3333-1234'}
+
+# modificando dados
+dados = {'nome': 'Guilherme', 'idade': 28, 'telefone': '3333-1234'}
+
+dados['nome']
+dados['idade']
+dados['telefone']
+
+dados['nome'] = 'Maria'
+dados['idade'] = 18
+dados['telefone'] = '9988-1781'
