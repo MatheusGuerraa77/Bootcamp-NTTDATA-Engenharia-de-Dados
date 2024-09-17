ef6fb2e4a3a571de6443e1c046d015937bcefe5a
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2024-09-17 00:08:24 -0300

    Update 01_desafio_bicicletaria.py

--- 06-POO/01_desafio_bicicletaria.py
+++ 06-POO/01_desafio_bicicletaria.py
@@ -16,6 +16,12 @@ class Bicicleta():
     def correr(self):
         print('Vrummmmmmm ...')
         
+    # def __str__(self):
+    #     return f'Bicicleta: cor={self.cor}, modelo={self.modelo}, ano={self.ano}, valor={self.valor}'
+    
+    def __str__(self): 
+        return f'{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}'
+        
         
 b1 = Bicicleta('vermelha', 'caloi', 2022, 600)
 b1.buzinar()
@@ -24,7 +30,7 @@ b1.parar()
 print(b1.cor, b1.modelo, b1.ano, b1.valor)
 
 b2 = Bicicleta('verde', 'monark', 2000, 189)
-Bicicleta.buzinar(b2)
+print(b2)
 
 
 
