d0d44478bcfb1dd7497a847a58565c4c5ada1d22
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2024-09-23 21:57:59 -0300

    commit update estrutura de dados

--- 01 - Estrutura de dados/01 - Listas/07_append.py
+++ 01 - Estrutura de dados/01 - Listas/07_append.py
@@ -3,5 +3,6 @@ lista = []
 lista.append(1)
 lista.append("Python")
 lista.append([40, 30, 20])
+lista.append('developer')
 
-print(lista)  # [1, "Python", [40, 30, 20]]
+print(lista)  # [1, "Python", [40, 30, 20], 'developer']
