ad267c2148e656fe7320807169426143aa4c9132
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2024-09-25 23:59:08 -0300

    Update 07_append.py

--- 01 - Estrutura de dados/01 - Listas/07_append.py
+++ 01 - Estrutura de dados/01 - Listas/07_append.py
@@ -4,5 +4,8 @@ lista.append(1)
 lista.append("Python")
 lista.append([40, 30, 20])
 lista.append('developer')
+lista.append('Data Engeneer')
+lista.append('back end developer')
 
-print(lista)  # [1, "Python", [40, 30, 20], 'developer']
+
+print(lista)  # [1, "Python", [40, 30, 20], 'developer', 'Data Engeneer', 'back end developer']
